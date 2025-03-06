3d39c25acbf3f44a9cc0652d03b283645e3d45ea
mireyargm7 <mireya.romerogarciamarquez@alum.uca.es>
2025-03-06 22:01:26 +0100

    fichero carmina
    te juro que soy informatica

--- Probar/main.c
+++ Probar/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main(){
-printf("Hola Mundo de Git");
-printf("Carmina donde has guardado los cables. Te juro que soy electricista y no informático");
+printf("Hola Mundo de Git\n");
+printf("Carmina donde has guardado los cables. Te juro que soy electricista y no informático\n");
+FILE *archivo=fopen("carmina","w");
+if(archivo==NULL){
+    printf("Error al crear el archivo\n");
+}else{
+    fwrite("hundir la flota", sizeof(char), 16, archivo);
+    fclose(archivo);
+}
+
+
 return 0;
 }
